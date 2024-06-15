38bf507c06f86f951d3e289fda6884ac37471d2c
Liam <liam@fascinated.cc>
2024-06-15 06:23:35 +0100

    cleanup config a bit

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,43 +5,40 @@ import (
 	"strconv"
 )
 
-// Gets the mongo connection string from the environment or returns a default value
+// GetMongoConnectionString returns the MongoDB connection string from the environment or a default value.
 func GetMongoConnectionString() string {
 	return getEnv("MONGO_URI", "mongodb://localhost:27017")
 }
 
-// Gets the paste ID length from the environment or returns a default value
+// GetPasteIDLength returns the paste ID length from the environment or a default value.
 func GetPasteIDLength() int {
-	lengthStr := getEnv("PASTE_ID_LENGTH", "12")
-	length, err := strconv.Atoi(lengthStr)
-	if err != nil {
-		// handle the error, e.g. return a default value
-		return 8
-	}
-	return length
+	return getIntEnv("PASTE_ID_LENGTH", 12)
 }
 
-// Gets the maximum paste length from the environment or returns a default value
+// GetMaxPasteLength returns the maximum paste length from the environment or a default value.
 func GetMaxPasteLength() int {
-	lengthStr := getEnv("MAX_PASTE_LENGTH", "5000000")
-	length, err := strconv.Atoi(lengthStr)
-	if err != nil {
-		// handle the error, e.g. return a default value
-		return 5000000
-	}
-	return length
-
+	return getIntEnv("MAX_PASTE_LENGTH", 5000000)
 }
 
-// Whether to enable metrics or not (default: false)
+// EnableMetrics returns whether to enable metrics or not (default: false).
 func EnableMetrics() bool {
 	return getEnv("ENABLE_METRICS", "false") == "true"
 }
 
-// Gets a key from the environment or returns a default value
+// getEnv returns the environment variable value or a default value if not set.
 func getEnv(key, defaultValue string) string {
 	if value, exists := os.LookupEnv(key); exists {
 		return value
 	}
 	return defaultValue
+}
+
+// getIntEnv returns the integer value of an environment variable or a default value if not set or invalid.
+func getIntEnv(key string, defaultValue int) int {
+	valueStr := getEnv(key, strconv.Itoa(defaultValue))
+	value, err := strconv.Atoi(valueStr)
+	if err != nil {
+		return defaultValue
+	}
+	return value
 }
