c816cf2045ad59ef9a5724b303ec3a51d66d5d8f
Liam <liam@fascinated.cc>
2024-07-11 02:29:25 +0100

    add avg line count metric

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -16,15 +16,22 @@ var totalPastesCounter = prometheus.NewGauge(
 		Help: "Total number of pastes",
 	})
 
-var avgPasteSize = prometheus.NewGauge(
+var avgPasteSizeCounter = prometheus.NewGauge(
 	prometheus.GaugeOpts{
 		Name: "avg_paste_size",
 		Help: "Average size of pastes",
 	})
 
+var avgLineCountCounter = prometheus.NewGauge(
+	prometheus.GaugeOpts{
+		Name: "avg_line_count",
+		Help: "Average number of lines in pastes",
+	})
+
 func RegisterMetrics() {
 	prometheus.Register(totalPastesCounter)
-	prometheus.Register(avgPasteSize)
+	prometheus.Register(avgPasteSizeCounter)
+	prometheus.Register(avgLineCountCounter)
 }
 
 // Starts the metrics updater (runs every 1 minute)
@@ -44,18 +51,24 @@ func updateMetrics() {
 
 	// todo: since this will grow and grow over time, we should probably
 	// use a count query instead but idk how to do that in prisma
-	pastes, err := prisma.GetPrismaClient().Paste.FindMany().Select(db.Paste.SizeBytes.Field()).Exec(ctx)
+	pastes, err := prisma.GetPrismaClient().Paste.FindMany().Select(
+		db.Paste.SizeBytes.Field(),
+		db.Paste.LineCount.Field(),
+	).Exec(ctx)
 	if err != nil {
 		fmt.Println("Error fetching pastes:", err)
 		return
 	}
 
-	avgSize := 0
+	avgSize, avgLineCount := 0, 0
 	for _, paste := range pastes {
 		avgSize += paste.SizeBytes
+		avgLineCount += paste.LineCount
 	}
 	avgSize /= len(pastes)
+	avgLineCount /= len(pastes)
 
 	totalPastesCounter.Set(float64(len(pastes)))
-	avgPasteSize.Set(float64(avgSize))
+	avgPasteSizeCounter.Set(float64(avgSize))
+	avgLineCountCounter.Set(float64(avgLineCount))
 }
