acc241c91a21be482e85ad941c8ede49c28a1182
Liam <liam@fascinated.cc>
2024-07-11 02:27:32 +0100

    oops

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -44,7 +44,7 @@ func updateMetrics() {
 
 	// todo: since this will grow and grow over time, we should probably
 	// use a count query instead but idk how to do that in prisma
-	pastes, err := prisma.GetPrismaClient().Paste.FindMany().Select(db.Paste.LineCount.Field()).Exec(ctx)
+	pastes, err := prisma.GetPrismaClient().Paste.FindMany().Select(db.Paste.SizeBytes.Field()).Exec(ctx)
 	if err != nil {
 		fmt.Println("Error fetching pastes:", err)
 		return
