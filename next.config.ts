8f1151b7a3cadaa3e1e82ea829ed8d55470767a3
Liam <liam@fascinated.cc>
2024-11-27 14:19:16 +0000

    enable react compiler and mem optimizations

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  experimental: {
+    reactCompiler: true,
+    webpackMemoryOptimizations: true,
+  },
+};
 
 export default nextConfig;
