493417f76862ea35ffc27867ca297f3524a26998
Liam <liam@fascinated.cc>
2024-11-27 13:14:54 +0000

    is this needed?

--- src/app/api/paste/[id]/route.ts
+++ src/app/api/paste/[id]/route.ts
@@ -1,6 +1,10 @@
 import { getPaste } from "@/app/common/prisma";
+import { NextRequest } from "next/server";
 
-export async function GET({ params }: { params: Promise<{ id: string }> }) {
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
   const id = (await params).id;
 
   const paste = await getPaste(id);
