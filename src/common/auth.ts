0f963ba3fd7d6a6541185782b84ea101de5150dc
Liam <liam@fascinated.cc>
2024-11-28 08:18:19 +0000

    fix auth?

--- src/common/auth.ts
+++ src/common/auth.ts
@@ -1,8 +1,11 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prismaClient } from "@/common/prisma";
+import { Config } from "./config";
 
 export const auth = betterAuth({
+  baseURL: Config.siteUrl,
+  trustedOrigins: [Config.siteUrl],
   database: prismaAdapter(prismaClient, {
     provider: "postgresql",
   }),
