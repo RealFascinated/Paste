278f3a940fb7637e6749984052ece16f363a3cd8
Liam <liam@fascinated.cc>
2024-11-28 01:52:41 +0000

    allow all in cors

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,23 @@ const nextConfig: NextConfig = {
     reactCompiler: true,
     webpackMemoryOptimizations: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withPlausibleProxy({
