5b69f62be4bdf2a9f9ef751da2b9e22913376750
Fascinated <liam@fascinated.cc>
2025-09-17 06:45:24 +0100

    fix

--- src/app/(pages)/raw/[id]/route.ts
+++ src/app/(pages)/raw/[id]/route.ts
@@ -1,11 +1,11 @@
-import { lookupPaste } from "@/common/utils/paste.util";
+import { getPaste } from "@/common/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(
   request: Request,
   { params }: { params: Promise<{ id: string }> }
 ) {
-  const paste = await lookupPaste((await params).id, true);
+  const paste = await getPaste((await params).id, true);
 
   if (paste == null) {
     return new NextResponse("Paste not found", { status: 404 });
